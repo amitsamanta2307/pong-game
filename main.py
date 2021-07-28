d6b8e5a8c82ab50ff010a62f98b513653a9b41d9
Amit Samanta <asamanta@infobase.com>
2021-07-28 10:20:36 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from turtle import Screen
+from paddle import Paddle
+
+screen = Screen()
+screen.setup(height=600, width=800)
+screen.title("Pong Game")
+screen.bgcolor("black")
+screen.tracer(0)
+
+
+l_paddle = Paddle((-350, 0))
+r_paddle = Paddle((350, 0))
+
+screen.listen()
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+
+screen.exitonclick()
