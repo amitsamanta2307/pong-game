a5d71efa7e4e91c469730035cfc677c863defde5
Amit Samanta <developer.amitsamanta@gmail.com>
2021-07-28 12:19:12 +0530

    Score keeping and changing the ball speed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(height=600, width=800)
@@ -13,6 +14,7 @@ screen.tracer(0)
 l_paddle = Paddle((-350, 0))
 r_paddle = Paddle((350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(l_paddle.go_up, "w")
@@ -23,7 +25,7 @@ screen.onkey(r_paddle.go_down, "Down")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     ball.move()
     screen.update()
 
@@ -38,8 +40,10 @@ while game_is_on:
     # Detect when right paddle misses
     if ball.xcor() > 380:
         ball.reset_position()
+        scoreboard.l_point()
 
     if ball.xcor() < -380:
         ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
